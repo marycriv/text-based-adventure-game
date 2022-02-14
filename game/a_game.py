16ffec0d79f77dfb7c13411349099388e92f3e84
marycriv <maryecrivelli@gmail.com>
2022-02-13 22:58:31 -0500

    chapter two unreachable

--- game/a_game.py
+++ game/a_game.py
@@ -1,11 +1,14 @@
 # a text based adventure game
 from b_title import *
-
 from c_chapter_one import *
+from d_chapter_two import *
 
 # start game
 # new_title = Title()
 # new_title.title_card()
 
-chapter_one = ChapterOne()
-chapter_one.scene_one()
+# chapter_one = ChapterOne()
+# chapter_one.scene_one()
+
+chapter_two = ChapterTwo()
+chapter_two.scene_one()
---
+++ game/d_chapter_two.py
@@ -0,0 +1,13 @@
+from b_title import *
+
+json_file_path = './dialogue.json'
+
+# with open(json_file_path, 'r') as j:
+#      contents = json.loads(j.read())
+
+# title_dialogue = contents["chapters"][2]["scenes"]
+
+class ChapterTwo:
+  
+  def scene_one(self):
+    print(hero.firstname)
