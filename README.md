0c28ac0e5716ea9b978b2b775baf6a99e282ef2d
marycriv <maryecrivelli@gmail.com>
2022-02-15 00:12:09 -0500

    chapter 1 outline added

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 A Labyrinth text based adventure game built in Python. 
 
+### Plot Progression:
+- **Prologue:** Events
+- **Chapter 1:** 
+    - *Scene 1:* Entering the Labyrinth
+    - *Scene 2:* Meeting Hoggle, faeries
+    - *Scene 3:* The endless brick corridor + the worm
+- **Chapter 2:**
+
 tutorial 1: https://www.youtube.com/watch?v=HzDcKq2NDwM
 
 music: https://www.youtube.com/watch?v=P1IBRTN89cg
--- game/d_chapter_two.py
+++ game/d_chapter_two.py
@@ -1,3 +1,7 @@
+import sys
+import os
+import climage
+
 from b_title import *
 
 json_file_path = './dialogue.json'
@@ -8,6 +12,14 @@ json_file_path = './dialogue.json'
 # title_dialogue = contents["chapters"][2]["scenes"]
 
 class ChapterTwo:
+
+  def prologue(self):
+    print("\n")
+    os.system('cls||clear')
+    game_functions.typewriter_print("CHAPTER TWO")
+    owl = climage.convert('./assets/logo.jpeg', is_unicode=True)
+    print(owl) 
+    self.scene_one()
   
   def scene_one(self):
     print("chapter two")
