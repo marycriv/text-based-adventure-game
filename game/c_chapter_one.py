ae603802651e5d71876e5e8ba4bb27e6f8441acd
marycriv <maryecrivelli@gmail.com>
2022-02-20 00:01:12 -0500

    commands moved to ch2

--- game/c_chapter_one.py
+++ game/c_chapter_one.py
@@ -14,19 +14,9 @@ dialogue = contents["chapters"][1]["scenes"]
 class ChapterOne:
 
     def next_chapter(self):
-        # game_functions.reset_console()
         game_functions.format_print("Next chapter function")
         chapter_two = ChapterTwo()
         chapter_two.scene_one()
-
-    def use_medkit(self):
-        if "medkit" in hero.items:
-            hero.items.remove("medkit")
-            game_functions.format_print("You used your medkit")
-            hero.health = 100
-            print(f"\nHealth: {hero.health}")
-        else: 
-            game_functions.format_print("You don't have a medkit.")
 
     def give_ring(self):
         if "ring" in hero.items:
@@ -34,31 +24,6 @@ class ChapterOne:
             game_functions.format_print("You give the goblin your ring.")
         else: 
             game_functions.format_print("You don't have a ring.")
-
-    # move to chapter two
-    def handle_goblin(self): 
-        goblin.move()
-        if hero.location == goblin.location:
-            goblin.talk()
-
-    def random_medkit(self):
-        medkit_find = random.choice([True, False])
-        medkit_find = False # for testing purposes
-        if medkit_find == True:
-            hero.items.append("medkit")
-            game_functions.format_print("You found a medkit!", 2)
-            print("Enter 'm' to use it!")
-
-    def random_bat_attack(self):
-        bat_attack = random.choice([True, False])
-        bat_attack = False # for testing purposes
-        if bat_attack == True:
-            game_functions.format_print("You were attacked by a bat", 2)
-            hero.health -= random.randint(1,100)
-            print(f"\nHealth: {hero.health}")
-            if hero.health == 0:
-                game_functions.format_print("You are dead!", 2)
-                game_functions.handle_game_over()
 
     def scene_one(self):
         hero.location = "gate"
@@ -76,8 +41,6 @@ class ChapterOne:
                 self.scene_two()
             elif action == "no":
                 game_functions.format_print(dialogue["one"][4])
-            elif action == "m": 
-                self.use_medkit()
             else: 
                 game_functions.format_print(dialogue["options"][0])
 
@@ -93,8 +56,6 @@ class ChapterOne:
                 self.scene_three()
             elif action == "no":
                 game_functions.format_print(dialogue["two"][2]) 
-            elif action == "m": 
-                self.use_medkit()
             else: 
                 game_functions.format_print(dialogue["options"][1])
 
@@ -109,8 +70,6 @@ class ChapterOne:
                 self.scene_four()
             elif action == "no":
                 game_functions.format_print(dialogue["three"][2]) 
-            elif action == "m": 
-                self.use_medkit()
             else: 
                 game_functions.format_print(dialogue["options"][1])
 
@@ -127,7 +86,5 @@ class ChapterOne:
                 self.next_chapter()
             elif action == "no":
                 game_functions.format_print(dialogue["four"][4]) 
-            elif action == "m": 
-                self.use_medkit()
             else: 
                 game_functions.format_print(dialogue["options"][1])
--- game/d_chapter_two.py
+++ game/d_chapter_two.py
@@ -20,6 +20,39 @@ class ChapterTwo:
     owl = climage.convert('./assets/logo.jpeg', is_unicode=True)
     print(owl) 
     self.scene_one()
+
+  def use_medkit(self):
+    if "medkit" in hero.items:
+        hero.items.remove("medkit")
+        game_functions.format_print("You used your medkit")
+        hero.health = 100
+        print(f"\nHealth: {hero.health}")
+    else: 
+        game_functions.format_print("You don't have a medkit.")
+
+  def handle_goblin(self): 
+    goblin.move()
+    if hero.location == goblin.location:
+        goblin.talk()
+
+  def random_medkit(self):
+    medkit_find = random.choice([True, False])
+    medkit_find = False # for testing purposes
+    if medkit_find == True:
+        hero.items.append("medkit")
+        game_functions.format_print("You found a medkit!", 2)
+        print("Enter 'm' to use it!")
+
+  def random_bat_attack(self):
+    bat_attack = random.choice([True, False])
+    bat_attack = False # for testing purposes
+    if bat_attack == True:
+        game_functions.format_print("You were attacked by a bat", 2)
+        hero.health -= random.randint(1,100)
+        print(f"\nHealth: {hero.health}")
+        if hero.health == 0:
+            game_functions.format_print("You are dead!", 2)
+            game_functions.handle_game_over()
   
   def scene_one(self):
     print("chapter two")
